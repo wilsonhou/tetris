01d5e839194c3bea92387c4c02a505dd7068fd0c
wilsonhou <wilshou@gmail.com>
2020-06-02 01:00:13 +1000

    added board skeleton methods

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Tetris
 
 Pygame Tetris game. In progress!
+
+### Structure and Features for MVP1
+
+- 10x20 with +2 height for new pieces
+- Death when any piece is detected above 20th thingo
+- 7 pieces (with reverses): L, T Z, I, O
+- 1 Square down per second (timer)
+- Space to place piece
+- When an entire line is filled, clear the line and increment count
+- os.clear to clear board for text based version
+ 
--- board.py
+++ board.py
@@ -1,7 +1,29 @@
 class Board:
   def __init__(self):
-    pass
+    self.score = 0 # Score is number of rows cleared
+    # * Board is 10x20 but you can create 10x24 to allow for more space
+    self.board = [
+      [' ' for _ in range(10)] for _ in range(24)
+    ]
   
   def is_over(self):
-    '''Retursn whether or not the game board is complete'''
+    '''Returns whether or not the game board is complete'''
+    # TODO: Figure out the lose condition. How do we check if game is over?
     pass
+
+  def is_full(self, row):
+    '''Returns whether or not a row is full'''
+    return all(item == '*' for item in row)
+
+  def reset_row(self, row):
+    '''Resets Row to Empty'''
+    if not is_full(row):
+      # Check row is complete first
+      return
+
+    # Get rid of full row
+    self.board.pop(self.board.index(row))
+    self.board.insert(0, [' ' for _ in range(10)])
+
+  def inc_score(self, count=1):
+    self.score += count
